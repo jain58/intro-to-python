0c9e218c0bedecab892786828d68a18492d9cb70
vaibhavjain <jain58@gmail.com>
2021-03-06 22:16:15 -0800

    random number generation in python

--- Problems/problem_4_dice_roll.py
+++ Problems/problem_4_dice_roll.py
@@ -1,7 +1,13 @@
 """
 Randomly returns two numbers between 1 and 6
 """
-
+import random
 # Generate two random integer between 1 and 6 (inclusive)
 
+
+number1 = random.randint(1,6)
+number2 = random.randint(1,6)
+
+print("Your first random number is "+str(number1))
+print("Your second random number is "+str(number2))
 # Tell the user what the result was
