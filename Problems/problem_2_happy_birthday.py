78e673a4b5c73ee048a1b738c142323a7da42361
vaibhavjain <jain58@gmail.com>
2021-03-07 23:39:00 -0800

    happy birthday code

--- Problems/problem_2_happy_birthday.py
+++ Problems/problem_2_happy_birthday.py
@@ -1,3 +1,9 @@
 """
 Ask for the user's name and then sing happy birthday to them.
 """
+
+home= input("What is your name? ")
+
+print("Happy Birthday to you "+str(home))
+print("Happy Birthday to you dear "+str(home))
+print("Happy Birthday to you ")
