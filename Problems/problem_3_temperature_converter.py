5355aa69cde5a72caf955a823d3fa492dbc1f604
vaibhavjain <jain58@gmail.com>
2021-03-07 23:29:26 -0800

    program to covert F into Celsius

--- Problems/problem_3_temperature_converter.py
+++ Problems/problem_3_temperature_converter.py
@@ -1,8 +1,17 @@
 """
 Given a temperature in Fahrenheit, return the temperature in Celsius
 """
-# Ask for a temperature in Fahrenheit
 
-# Calculate it in Celsius
+while True:
+    # Ask for a temperature in Fahrenheit
+    temp_F = input("Enter temp in F ")
+    print("Temp in F " + str(temp_F))
 
-# Tell the user what it is
+    # C = (F-32)*5/9
+
+    # Calculate it in Celsius
+    temp_F = float(temp_F)
+    temp_C = (temp_F-32)*5/9
+
+    print("Temp in C "+str(temp_C))
+    # Tell the user what it is
